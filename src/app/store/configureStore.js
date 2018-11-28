3ba1fc03b53232b2daefb0c0db011e75b2d21446
Richard <hyacinthzy1@gmail.com>
2018-11-28 15:09:50 +1100

    thunk structure added and async action set for loading

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,29 +1,26 @@
-import { createStore, applyMiddleware} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '../reducers/rootReducer';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "../reducers/rootReducer";
+import thunk from "redux-thunk";
 
-export const configureStore = (preloadedState) => {
-    const middlewares = [];
-    const middlewareEnhancer = applyMiddleware(...middlewares);
+export const configureStore = preloadedState => {
+  const middlewares = [thunk];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
 
-    const storeEnhancers = [middlewareEnhancer];
-    
-    const composedEnhancer = composeWithDevTools(...storeEnhancers);
+  const storeEnhancers = [middlewareEnhancer];
 
-    const store = createStore(
-        rootReducer,
-        preloadedState,
-        composedEnhancer
-    );
+  const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
-    if (process.env.NODE_ENV !== 'production') {
-        if (module.hot) {
-            module.hot.accept('../reducers/rootReducer', () => {
-                const newRootReducer = require('../reducers/rootReducer').default;
-                store.replaceReducer(newRootReducer);
-            })
-        }
+  const store = createStore(rootReducer, preloadedState, composedEnhancer);
+
+  if (process.env.NODE_ENV !== "production") {
+    if (module.hot) {
+      module.hot.accept("../reducers/rootReducer", () => {
+        const newRootReducer = require("../reducers/rootReducer").default;
+        store.replaceReducer(newRootReducer);
+      });
     }
-    
-    return store;
-}
+  }
+
+  return store;
+};
